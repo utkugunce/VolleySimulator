6cfade3b8084e9368ab6d1dcafc9a4a4e4198e27
Utku Günçe <utkugunce@gmail.com>
2025-12-31 13:41:49 +0300

    Added Google Analytics tracking code

--- VolleySimulator-main/app/layout.tsx
+++ VolleySimulator-main/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -77,6 +78,21 @@ export default function RootLayout({
         />
         <Analytics />
         <SpeedInsights />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C4T368HQ54"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C4T368HQ54');
+          `}
+        </Script>
       </body>
     </html>
   );
